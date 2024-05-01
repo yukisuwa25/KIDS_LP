ee656ab58fa4c7bd026bf45d033bd87ef1d8853a
yukisuwa25 <yuki.997.777@gmail.com>
2024-05-01 17:56:57 +0900

    追記

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./js/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./product.html", "./js/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
